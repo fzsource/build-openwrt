6ee8930cf302ef2ba523261669994b94cae594f9
fzsource <63889910+fzsource@users.noreply.github.com>
2025-01-01 23:19:59 +0800

    Update diy-part1.sh

--- config/lede-n1-fw876-xiaorouji/diy-part1.sh
+++ config/lede-n1-fw876-xiaorouji/diy-part1.sh
@@ -17,3 +17,11 @@
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
+## 解除系统限制
+ulimit -u 10000
+ulimit -n 4096
+ulimit -d unlimited
+ulimit -m unlimited
+ulimit -s unlimited
+ulimit -t unlimited
+ulimit -v unlimited
